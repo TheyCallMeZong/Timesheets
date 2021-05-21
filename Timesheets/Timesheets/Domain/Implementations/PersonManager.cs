2407aea7e2651c07c96ea0f7f018d171da30c5f0
Семён Савченко <savhenko10@mail.ru>
2021-05-21 14:35:25 +0700

    timesheets

---
+++ Timesheets/Timesheets/Domain/Implementations/PersonManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Timesheets.Domain.Interfaces;
+using Timesheets.Models;
+using Timesheets.Models.Dto;
+
+namespace Timesheets.Domain.Implementations
+{
+    public class PersonManager : IPersonManager
+    {
+        private PersonsDb _persons;
+
+        public PersonManager()
+        {
+            _persons = new PersonsDb();
+        }
+        public Person GetPersonById(int id)
+        {
+            Person result = _persons.Persons.FirstOrDefault(person => person.Id == id);
+            return result;
+        }
+
+        public List<Person> GetPersonsByName(string name)
+        {
+            var result = _persons.Persons.Where(person => person.FirstName == name).ToList();
+            return result;
+        }
+
+        public List<Person> TakePersons(int skip, int take)
+        {
+            var result = _persons.Persons.Skip(skip).Take(take).ToList();
+            return result;
+        }
+
+        public void CreatePerson(PersonDto item)
+        {
+            int id = _persons.GetCount();
+            var newPerson = new Person()
+            {
+                Id = ++id,
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Email = item.Email,
+                Company = item.Company,
+                Age = item.Age
+            };
+            _persons.Persons.Add(newPerson);
+        }
+
+        public void UpdatePerson(Person item)
+        {
+            var updatePerson = _persons.Persons.FirstOrDefault(person => person.Id == item.Id);
+            if (updatePerson != null)
+            {
+                updatePerson.FirstName = item.FirstName;
+                updatePerson.LastName = item.LastName;
+                updatePerson.Email = item.Email;
+                updatePerson.Company = item.Company;
+                updatePerson.Age = item.Age;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            _persons.Persons.RemoveAll(person => person.Id == id);
+        }
+    }
+}
--- Timesheets/Timesheets/Startup.cs
+++ Timesheets/Timesheets/Startup.cs
@@ -5,17 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Timesheets.Domain.Implementations;
+using Timesheets.Domain.Interfaces;
 
 namespace Timesheets
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IPersonManager, PersonManager>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Timesheets"
+                });
+            });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,13 +44,22 @@ namespace Timesheets
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger(c =>
+                {
+                    c.SerializeAsV2 = true;
+                });
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Timesheets");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
